96e4b841ae8fd5c3308e8eddc244db99789272bc
robandrade <103383943+robandrade@users.noreply.github.com>
2022-04-28 00:45:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿// Corrected by Robert Andrade Sanchez.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CST117_IC08_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4)); B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+        }
+    }
+
+    class Set
+    {
+        private List<int> elements;
+        public Set()
+        {
+            elements = new List<int>();
+        }
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                // got rid of the else statement that had the return false,
+                // do not need it just the one outside the {}
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+        public Set union(Set rhs)
+        {
+            // created a new set named val
+            Set val = new Set();
+            for (int i = 0; i < rhs.elements.Count; i++)
+            { 
+                this.addElement(rhs.elements[i]);
+
+            }
+            // created another for loop with the new set created, to this union val or A union B
+            for (int i = 0;i < this.elements.Count; i++)
+            {
+
+                val.addElement(this.elements[i]);
+            }
+            return val;
+        }
+    }
+    
+}
+
